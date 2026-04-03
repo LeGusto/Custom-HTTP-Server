16e9ba69d140ed3a9982f7b77c2ec059d3d0d131
= <janusonis.gustas@gmail.com>
2026-04-03 23:31:13 +0300

    float unpacker

--- src/float_serializer.cpp
+++ src/float_serializer.cpp
@@ -6,7 +6,7 @@ uint64_t pack754(long double f, uint32_t bits, uint32_t exp_bits)
 {
     if (f == 0)
         return 0;
-    uint32_t bias = (1 << (exp_bits - 1)) - 1;
+    uint32_t bias = (1ULL << (exp_bits - 1)) - 1;
     uint64_t sign = (f < 0);
 
     long double f_tmp = std::abs(f);
@@ -28,17 +28,46 @@ uint64_t pack754(long double f, uint32_t bits, uint32_t exp_bits)
     uint64_t significand_bits = bits - exp_bits - 1;
     long double scaled_f = f_tmp * (1LL << significand_bits);     // convert to long long
     uint64_t significand = static_cast<uint64_t>(scaled_f + 0.5); // rounds to the nearest int
-
+    // std::cout << exp << "buh\n";
     exp += bias;
 
     uint64_t ans = (sign << (bits - 1));
     ans |= (static_cast<uint64_t>(exp) << (significand_bits));
     ans |= significand;
 
+    // std::cout << sign << " " << exp << " " << significand << "\n";
+    // std::cout << ans << " huh\n";
     return ans;
 }
 
-int main()
+long double unpack754(uint64_t encoded_f, uint32_t bits, uint32_t exp_bits)
 {
-    std::cout << std::hex << pack754(1.5, 64, 11) << "\n";
+    // std::cout << encoded_f << "\n";
+    uint64_t bias = (1 << (exp_bits - 1)) - 1;
+    uint64_t significand_bits = bits - exp_bits - 1;
+    uint64_t sign = ((1ULL << (bits - 1)) & encoded_f) > 0;
+    uint64_t significand = (encoded_f & ((1ULL << significand_bits) - 1));
+    int64_t exp = ((encoded_f >> significand_bits) & ((1 << exp_bits) - 1));
+    exp -= bias;
+
+    // std::cout << sign << " " << exp << " " << significand << "\n";
+
+    long double f = static_cast<long double>(significand) / (1LL << significand_bits);
+    f += 1;
+
+    while (exp < 0)
+    {
+        f /= 2;
+        exp++;
+    }
+    while (exp > 0)
+    {
+        f *= 2;
+        exp--;
+    }
+
+    if (sign)
+        f *= -1;
+
+    return f;
 }
