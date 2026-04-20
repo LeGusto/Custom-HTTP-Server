851c867dea5c12d305596a58e04925621ff07873
= <janusonis.gustas@gmail.com>
2026-04-20 23:28:36 +0300

    simple latency monitoring system for request processing and config for user requests per thread

---
+++ build-release/CMakeFiles/run_user.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run_user"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/run_user.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build-release/CMakeFiles/test_serializer.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "/home/LeGusto/Documents/Custom-HTTP-Server/bin/test_serializer"
+  "/home/LeGusto/Documents/Custom-HTTP-Server/bin/test_serializer.pdb"
+  "CMakeFiles/test_serializer.dir/link.d"
+  "CMakeFiles/test_serializer.dir/src/serializer.cpp.o"
+  "CMakeFiles/test_serializer.dir/src/serializer.cpp.o.d"
+  "CMakeFiles/test_serializer.dir/tests/test_serializer.cpp.o"
+  "CMakeFiles/test_serializer.dir/tests/test_serializer.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_serializer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
