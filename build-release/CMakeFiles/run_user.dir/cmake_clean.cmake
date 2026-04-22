9bf11052fa368ddc459c7bf5f15de1a18495e90d
= <janusonis.gustas@gmail.com>
2026-04-23 00:01:37 +0300

    stopped tracking builds

--- build-release/CMakeFiles/run_user.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/run_user"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/run_user.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
